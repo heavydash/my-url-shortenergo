b1af07d384dd8e07e2cd5794d74830297667b6a7
Artem <artem_lysak07@mail.ru>
2025-10-01 13:54:05 +0300

    Update handlers.go with constants and lower go.mod to 1.24 for iter2

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/heavydash/my-url-shortenergo
 
-go 1.25.0
+go 1.24.0
 
 require github.com/stretchr/testify v1.11.1
 
--- internal/handler/handlers.go
+++ internal/handler/handlers.go
@@ -23,45 +23,45 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		defer r.Body.Close()
 		body, err := io.ReadAll(r.Body)
 		if err != nil {
-			http.Error(w, "Failed to read body", 400)
+			http.Error(w, "Failed to read body", http.StatusBadRequest)
 			return
 		}
 		urlString := string(body)
 		if len(urlString) == 0 || !strings.HasPrefix(urlString, "http") {
-			http.Error(w, "Invalid URL", 400)
+			http.Error(w, "Invalid URL", http.StatusBadRequest)
 			return
 		}
 		urlmodel := model.URLModel{URL: urlString}
 		savedModel, err := h.repo.SaveURL(urlmodel)
 		if err != nil {
-			http.Error(w, "Failed to save URL", 400)
+			http.Error(w, "Failed to save URL", http.StatusBadRequest)
 			return
 		}
 		w.Header().Set("content-type", "text/plain")
 		w.WriteHeader(http.StatusCreated)
 		_, err = w.Write([]byte("http://localhost:8080/" + savedModel.ID))
 		if err != nil {
-			http.Error(w, "Write error", 500)
+			http.Error(w, "Write error", http.StatusInternalServerError)
 			return
 		}
 	} else if r.Method == http.MethodGet {
 		id := strings.TrimPrefix(r.URL.Path, "/")
 		if len(id) == 0 {
-			http.Error(w, "Invalid ID", 400)
+			http.Error(w, "Invalid ID", http.StatusBadRequest)
 			return
 		}
 		urlmodel, err := h.repo.GetURL(id)
 		if err != nil {
 			if strings.Contains(err.Error(), "not found") {
-				http.Error(w, "ID not found", 400)
+				http.Error(w, "ID not found", http.StatusBadRequest)
 			} else {
-				http.Error(w, "Method not allowed", 500)
+				http.Error(w, "Method not allowed", http.StatusInternalServerError)
 			}
 			return
 		}
 		http.Redirect(w, r, urlmodel.URL, http.StatusTemporaryRedirect)
 	} else {
-		http.Error(w, "Method not allowed", 405)
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
 }
