d64f1c3afd57d9c9f40fed11f3b321034c2609a3
Artem <artem_lysak07@mail.ru>
2025-10-12 17:00:13 +0300

    Configuration for the URL shortener service using environment variables

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,7 +3,10 @@ package config
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/url"
 	"os"
+	"strings"
 )
 
 type Config struct {
@@ -16,18 +19,48 @@ func NewConfig() (*Config, error) {
 		ServerAddr: "localhost:8080",
 		BaseURL:    "https://localhost:8080",
 	}
-	flag.StringVar(&cfg.ServerAddr, "a", cfg.ServerAddr, "address to run HTTP server")
-	flag.StringVar(&cfg.BaseURL, "b", cfg.BaseURL, "base URL for shortened links")
-	flag.Parse()
 
-	if addr, exists := os.LookupEnv("SERVER_ADDR"); exists && addr != "" {
+	fs := flag.NewFlagSet("my-url-shortener", flag.ContinueOnError)
+	fs.StringVar(&cfg.ServerAddr, "a", cfg.ServerAddr, "address to run HTTP server")
+	fs.StringVar(&cfg.BaseURL, "b", cfg.BaseURL, "base URL for shortened links")
+
+	args := make([]string, 0)
+	for i := 1; i < len(os.Args); i++ {
+		if os.Args[i] == "-a" || os.Args[i] == "-b" {
+			args = append(args, os.Args[i])
+			if i+1 < len(os.Args) {
+				args = append(args, os.Args[i+1])
+				i++
+			}
+		}
+	}
+	if err := fs.Parse(args); err != nil {
+		return nil, fmt.Errorf("parsing flags: %w", err)
+	}
+	if addr, exists := os.LookupEnv("SERVER_ADDRESS"); exists && addr != "" {
+		log.Printf("Using server address: %s", addr)
 		cfg.ServerAddr = addr
+	} else {
+		log.Printf("Using default server address: %s", cfg.ServerAddr)
 	}
-	if url, exists := os.LookupEnv("BASE_URL"); exists && url != "" {
-		cfg.BaseURL = url
+	if URL, exists := os.LookupEnv("BASE_URL"); exists && URL != "" {
+		log.Printf("Using base URL: %s", URL)
+		cfg.BaseURL = URL
+	} else {
+		log.Printf("Using default base URL: %s", cfg.BaseURL)
 	}
+
 	if cfg.BaseURL == "" {
 		cfg.BaseURL = fmt.Sprintf("https://%s/", cfg.ServerAddr)
+	}
+	u, err := url.Parse(cfg.BaseURL)
+	if err != nil || u.Scheme == "" || u.Host == "" {
+		log.Printf("Using default base URL: %s", cfg.BaseURL)
+		cfg.BaseURL = "http://localhost:8080/"
+	}
+	if !strings.Contains(cfg.ServerAddr, ":") {
+		log.Printf("Invalid ServerAddr, Using default server address: %s", cfg.ServerAddr)
+		cfg.ServerAddr = "localhost:8080"
 	}
 	return cfg, nil
 }
---
+++ internal/config/config_test.go
@@ -0,0 +1,81 @@
+package config
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"os"
+	"testing"
+)
+
+func TestNewConfig(t *testing.T) {
+	///Переменные окружения заданы
+	t.Run("env variables set", func(t *testing.T) {
+		os.Setenv("SERVER_ADDRESS", ":9090")
+		os.Setenv("BASE_URL", "https://example.com")
+		defer os.Unsetenv("SERVER_ADDRESS")
+		defer os.Unsetenv("BASE_URL")
+
+		originalArgs := os.Args
+		os.Args = []string{"test", "-a", ":8080", "-b", "https://test.com"}
+		defer func() { os.Args = originalArgs }()
+
+		cfg, err := NewConfig()
+		require.NoError(t, err)
+		assert.Equal(t, ":9090", cfg.ServerAddr)
+		assert.Equal(t, "https://example.com", cfg.BaseURL)
+	})
+	//Переменные не заданы, флаги заданы
+	t.Run("flags override env variables", func(t *testing.T) {
+		originalArgs := os.Args
+		os.Args = []string{"test", "-a", ":8081", "-b", "https://test.com"}
+		defer func() { os.Args = originalArgs }()
+
+		cfg, err := NewConfig()
+		require.NoError(t, err)
+		assert.Equal(t, ":8081", cfg.ServerAddr)
+		assert.Equal(t, "https://test.com", cfg.BaseURL)
+	})
+	//Ничего не задано
+	t.Run("default values", func(t *testing.T) {
+		originalArgs := os.Args
+		os.Args = []string{"test"}
+		defer func() { os.Args = originalArgs }()
+
+		cfg, err := NewConfig()
+		require.NoError(t, err)
+		assert.Equal(t, "localhost:8080", cfg.ServerAddr)
+		assert.Equal(t, "https://localhost:8080", cfg.BaseURL)
+	})
+	//Пустые переменные окружения
+	t.Run("env are empty", func(t *testing.T) {
+		os.Setenv("SERVER_ADDRESS", "")
+		os.Setenv("BASE_URL", "")
+		defer os.Unsetenv("SERVER_ADDRESS")
+		defer os.Unsetenv("BASE_URL")
+
+		originalArgs := os.Args
+		os.Args = []string{"test", "-a", ":8081", "-b", "https://test.com"}
+		defer func() { os.Args = originalArgs }()
+
+		cfg, err := NewConfig()
+		require.NoError(t, err)
+		assert.Equal(t, ":8081", cfg.ServerAddr)
+		assert.Equal(t, "https://test.com", cfg.BaseURL)
+	})
+	//Валидация
+	t.Run("Base URL is empty", func(t *testing.T) {
+		os.Setenv("SERVER_ADDRESS", ":9090")
+		os.Setenv("BASE_URL", "")
+		defer os.Unsetenv("SERVER_ADDRESS")
+		defer os.Unsetenv("BASE_URL")
+
+		originalArgs := os.Args
+		os.Args = []string{"test"}
+		defer func() { os.Args = originalArgs }()
+
+		cfg, err := NewConfig()
+		require.NoError(t, err)
+		assert.Equal(t, ":9090", cfg.ServerAddr)
+		assert.Equal(t, "https://localhost:8080", cfg.BaseURL)
+	})
+}
