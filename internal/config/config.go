8cc29b7c70a0db498489a267b17c2a1f25d6ab6b
Artem <artem_lysak07@mail.ru>
2025-10-16 11:22:23 +0300

    Implement Increment 5: Add env and flags config

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,6 +3,10 @@ package config
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/url"
+	"os"
+	"strings"
 )
 
 type Config struct {
@@ -11,16 +15,52 @@ type Config struct {
 }
 
 func NewConfig() (*Config, error) {
-	cfg := &Config{}
-	flag.StringVar(&cfg.ServerAddr, "a", "localhost:8080", "address to run HTTP server")
-	flag.StringVar(&cfg.BaseURL, "b", "http://localhost:8080/", "base URL for shortened links")
-	flag.Parse()
-
-	if cfg.ServerAddr == "" {
-		return nil, fmt.Errorf("server address (-a) is required")
+	cfg := &Config{
+		ServerAddr: "localhost:8080",
+		BaseURL:    "https://localhost:8080",
 	}
+
+	fs := flag.NewFlagSet("my-url-shortener", flag.ContinueOnError)
+	fs.StringVar(&cfg.ServerAddr, "a", cfg.ServerAddr, "address to run HTTP server")
+	fs.StringVar(&cfg.BaseURL, "b", cfg.BaseURL, "base URL for shortened links")
+
+	args := make([]string, 0)
+	for i := 1; i < len(os.Args); i++ {
+		if os.Args[i] == "-a" || os.Args[i] == "-b" {
+			args = append(args, os.Args[i])
+			if i+1 < len(os.Args) {
+				args = append(args, os.Args[i+1])
+				i++
+			}
+		}
+	}
+	if err := fs.Parse(args); err != nil {
+		return nil, fmt.Errorf("parsing flags: %w", err)
+	}
+	if addr, exists := os.LookupEnv("SERVER_ADDRESS"); exists && addr != "" {
+		log.Printf("Using server address: %s", addr)
+		cfg.ServerAddr = addr
+	} else {
+		log.Printf("Using default server address: %s", cfg.ServerAddr)
+	}
+	if URL, exists := os.LookupEnv("BASE_URL"); exists && URL != "" {
+		log.Printf("Using base URL: %s", URL)
+		cfg.BaseURL = URL
+	} else {
+		log.Printf("Using default base URL: %s", cfg.BaseURL)
+	}
+
 	if cfg.BaseURL == "" {
 		cfg.BaseURL = fmt.Sprintf("https://%s/", cfg.ServerAddr)
+	}
+	u, err := url.Parse(cfg.BaseURL)
+	if err != nil || u.Scheme == "" || u.Host == "" {
+		log.Printf("Using default base URL: %s", cfg.BaseURL)
+		cfg.BaseURL = "http://localhost:8080/"
+	}
+	if !strings.Contains(cfg.ServerAddr, ":") {
+		log.Printf("Invalid ServerAddr, Using default server address: %s", cfg.ServerAddr)
+		cfg.ServerAddr = "localhost:8080"
 	}
 	return cfg, nil
 }
