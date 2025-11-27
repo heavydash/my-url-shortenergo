97f7f34bd8812117c30e0bf3d6a799d790ad7a84
Artem <artem_lysak07@mail.ru>
2025-11-27 23:51:01 +0300

    fix: run goose migrations from migrations package

--- migrations/migration.go
+++ migrations/migration.go
@@ -1,21 +1,16 @@
 package migrations
 
 import (
-	"embed"
 	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/pressly/goose/v3"
 )
 
-var embedMigrations embed.FS
-
 func RunMigrations(dsn string) error {
-	goose.SetBaseFS(embedMigrations)
-
-	db, err := goose.OpenDBWithDriver("postgres", dsn)
+	db, err := goose.OpenDBWithDriver("pgx", dsn)
 	if err != nil {
 		return err
 	}
 	defer db.Close()
 
-	return goose.Up(db, "migrations")
+	return goose.Up(db, ".")
 }
