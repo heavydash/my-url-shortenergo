c201b08ee069da377cdc40e16f20cb51bd233059
Artem <artem_lysak07@mail.ru>
2025-11-28 05:10:19 +0300

    fix gzip

--- internal/middleware/gzip.go
+++ internal/middleware/gzip.go
@@ -2,59 +2,34 @@ package middleware
 
 import (
 	"compress/gzip"
-	"log"
 	"net/http"
 	"strings"
 )
 
-type gzipResponseWriter struct {
-	w           http.ResponseWriter
-	gw          *gzip.Writer
-	wroteHeader bool
+type gzipWriter struct {
+	http.ResponseWriter
+	writer *gzip.Writer
 }
 
-func (grw *gzipResponseWriter) Write(p []byte) (int, error) {
-	if !grw.wroteHeader {
-		grw.WriteHeader(http.StatusOK)
-	}
-	return grw.gw.Write(p)
-}
-
-func (grw *gzipResponseWriter) WriteHeader(statusCode int) {
-	if !grw.wroteHeader {
-		grw.w.Header().Set("Content-Encoding", "gzip")
-		grw.w.WriteHeader(statusCode)
-		grw.wroteHeader = true
-		return
-	}
-}
-
-func (grw *gzipResponseWriter) Header() http.Header {
-	return grw.w.Header()
+func (w *gzipWriter) Write(b []byte) (int, error) {
+	return w.writer.Write(b)
 }
 
 func GzipMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Header.Get("Content-Encoding") == "gzip" {
-			cmp, err := gzip.NewReader(r.Body)
-			if err != nil {
-				log.Printf("Failed to create gzip reader: %v", err)
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			r.Body = cmp
-			defer cmp.Close()
-		}
-		if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
-			gw := gzip.NewWriter(w)
-			grw := &gzipResponseWriter{w: w, wroteHeader: false, gw: gw}
-			defer grw.gw.Close()
-
-			log.Printf("Compressed gzip response")
-			next.ServeHTTP(grw, r)
-			defer grw.gw.Close()
-		} else {
+		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
 			next.ServeHTTP(w, r)
+			return
 		}
+
+		gz, err := gzip.NewWriterLevel(w, gzip.BestSpeed)
+		if err != nil {
+			next.ServeHTTP(w, r)
+			return
+		}
+		defer gz.Close()
+
+		w.Header().Set("Content-Encoding", "gzip")
+		next.ServeHTTP(&gzipWriter{ResponseWriter: w, writer: gz}, r)
 	})
 }
