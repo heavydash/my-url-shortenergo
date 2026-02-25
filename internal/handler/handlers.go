046078275181a6c8b5e7ba48dac63848f652d4a9
Artem <artem_lysak07@mail.ru>
2026-02-25 18:49:08 +0300

    fix: remove duplicated imports in handlers.go after merge iter18

--- internal/handler/handlers.go
+++ internal/handler/handlers.go
@@ -29,18 +29,18 @@ import (
 	"strings"
 
 	"github.com/google/uuid"
+
 	"github.com/heavydash/my-url-shortenergo/internal/audit"
 	"github.com/heavydash/my-url-shortenergo/internal/audit/service"
-	"github.com/heavydash/my-url-shortenergo/internal/deleter"
-	"github.com/heavydash/my-url-shortenergo/internal/middleware"
-	"github.com/heavydash/my-url-shortenergo/internal/util"
-
 	"github.com/heavydash/my-url-shortenergo/internal/config"
+	"github.com/heavydash/my-url-shortenergo/internal/deleter"
 	"github.com/heavydash/my-url-shortenergo/internal/idgen"
+	"github.com/heavydash/my-url-shortenergo/internal/middleware"
 	"github.com/heavydash/my-url-shortenergo/internal/model"
-	"go.uber.org/zap"
-
 	"github.com/heavydash/my-url-shortenergo/internal/repository"
+	"github.com/heavydash/my-url-shortenergo/internal/util"
+	_ "github.com/joho/godotenv/autoload"
+	"go.uber.org/zap"
 )
 
 // Handler содержит зависимости для обработки HTTP запросов сервиса сокращения URL.
